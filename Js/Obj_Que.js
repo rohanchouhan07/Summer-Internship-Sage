cad68728856c00b859ed089b90ee023cb0f23a1b
rohanchouhan07 <rohanchouhan.sage@gmail.com>
2026-06-17 12:27:38 +0530

    28

---
+++ Js/Obj_Que.js
@@ -0,0 +1,37 @@
+// !Que 1
+console.log("Que=1")
+const names=["gagan","ishan","yogesh"]
+let r = names.map(name=>name.toUpperCase())
+console.log(r,"\n")
+
+// !Que 2
+console.log("Que=2")
+const products=[
+    {id:1,name:"Laptop"},
+    {id:2,name:"Phone"},
+    {id:3,name:"Tablet"}
+]
+const pd=products.map(x=>x.name)
+console.log(pd,"\n")
+
+// !Que 3
+console.log("Que 3")
+const word=["apple","banana","cat","elephnt","dog"];
+let wd=word.filter(x=>x.length >5)
+console.log(wd,"\n")
+
+// !Que 4
+console.log("Que 4")
+const users=[
+    {name:"A",age:16},
+    {name:"B",age:22},
+    {name:"C",age:19}
+]
+// let ur=users.filter(x=>x.age>18).map(v=>v.name);
+let ur=users.filter(x=>x.age>18);
+console.log(ur,"\n");
+
+// !Que 5
+console.log("Que 5");
+const  
+console.log(,"\n");
